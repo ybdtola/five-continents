5fd920d41bdbf006d85df5e555b8884b1af7d56d
Adetola <ybdtola1@gmail.com>
2021-11-15 17:43:56 +0100

    Account Page

--- src/views/Account.vue
+++ src/views/Account.vue
@@ -1,10 +1,33 @@
 <template>
-  
+<Navbar :count='cart' />  
+<HeroBanner/>
+  <Form/>
 </template>
 
 <script>
-export default {
+import Navbar from '../components/Navbar.vue'
+import HeroBanner from '../components/HeroBanner.vue'
+import Form from '../components/Form.vue'
 
+export default {
+    components: {
+        Navbar, Form, HeroBanner
+    },
+    data(){
+        return{
+            cart: 0
+        }
+    },
+    created(){
+            let cart = []
+               if(JSON.parse(localStorage.getItem('menu'))){
+                   cart = JSON.parse(localStorage.getItem('menu'))
+                   this.cart = cart.length
+               }else{
+                   cart = []
+                    this.cart = cart.length
+               }
+    }
 }
 </script>
 
