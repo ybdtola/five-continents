41016fafdf8cfa6c17642c7f3bb376f72b2d5f37
Adetola <ybdtola1@gmail.com>
2021-11-30 12:40:48 +0100

    Added FontAwesome

--- src/main.js
+++ src/main.js
@@ -1,9 +1,21 @@
+
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import './assets/main.css'
 
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import specific icons */
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(faShoppingCart)
+
+// Vue.config.productionTip = false
 
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).component("font-awesome-icon", FontAwesomeIcon).use(store).use(router).mount('#app')
